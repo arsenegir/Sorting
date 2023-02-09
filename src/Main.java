bae35b6db68ea7f46fbd34892501d2cf88100050
g21230555 <arsene.giriteka@etu.univ-amu.fr>
2023-02-09 09:29:56 +0100

    cmplet

--- src/Main.java
+++ src/Main.java
@@ -5,23 +5,23 @@ import java.util.Random;
 public class Main {
     public static void main(String[] args) {
         List<Integer> integers = new ArrayList<>();
-        integers = fillWithRandom(integers,1000,8,0);
+        integers = fillWithRandom(integers,10,8,0);
 
         System.out.println("Before: "+integers.toString());
 
-        integers = MergeSorting.sort(integers);
-        //integers = BublleSorting.sort(integers);
-        //integers = QuickSorting.sort(integers,integers.get(9),integers.get(0));
-        //integers = BublleSorting.sort(integers);
-        //integers = HeapSorting.sort(integers);
-        //integers = ByBaseSorting.sort(integers); not working yet
+        //integers = MergeSorting.sort(integers); //does work
+        //integers = BublleSorting.sort(integers); //does work
+        integers = QuickSorting.sort(integers,integers.get(9),integers.get(0)); //bogus
+        //integers = BublleSorting.sort(integers); // does work
+        //integers = HeapSorting.sort(integers); //does work
+        //integers = ByBaseSorting.sort(integers); //bogus
 
         System.out.println("After: "+integers.toString());
     }
 
     public static List<Integer> fillWithRandom(List<Integer> list,int length,int max,int min){
         for (int i = 0;i<length;i++){
-            list.add(Integer.valueOf(new Random().nextInt(max-min)+min));
+            list.add(new Random().nextInt(max - min) + min);
         }
         return list;
     }
